75c81ed6a5eeb4fb88f0826090fd2b3d0cb3df06
Kassidy Webb <ktwebb@crimson.ua.edu>
2020-01-20 10:17:03 -0600

    j

---
+++ docs/js/chapters.js
@@ -0,0 +1,7 @@
+(function($) {
+    $(function() { 
+        $("#header").load("../../header.html");
+        $("#sidebar").load("../../sidebar.html");
+        $("#footer").load("../../footer.html");
+    });
+  })(jQuery); 
